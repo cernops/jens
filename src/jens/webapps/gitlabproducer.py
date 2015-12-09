c209d5461602f84905867a040dfb9ff149e1889d
Alberto Peon <alberto.rodriguez.peon@cern.ch>
2015-12-09 10:50:28 +0100

    Add License agreement to gitlabproducer.py

--- src/jens/webapps/gitlabproducer.py
+++ src/jens/webapps/gitlabproducer.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# Copyright (C) 2015, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
 import fcntl
 import logging
 import optparse
