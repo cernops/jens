858c4947e64eb202903a9c7dbfc18de7f6280cb7
Alberto Peon <alberto.rodriguez.peon@cern.ch>
2015-12-15 16:20:15 +0100

    Fix error message

--- src/jens/webapps/gitlabproducer.py
+++ src/jens/webapps/gitlabproducer.py
@@ -49,7 +49,7 @@ def hello_gitlab():
         enqueue_hint(settings, partition, name)
         return 'OK'
     except JensMessagingError as error:
-        logging.error("%s-%s couldn't be added to the Queue")
+        logging.error("%s-%s couldn't be added to the Queue (%s)" % (partition, name, str(error)))
         return 'Queue not accessible', 500
     except NameError as error:
         logging.error("'%s' couldn't be found in repositories" % (url))
