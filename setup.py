c8d48e50b6ed4c5246eca865e8e982ad3c3fee2e
Nacho Barrientos <nacho.barrientos@cern.ch>
2018-03-26 16:50:01 +0200

    Correct formatting and indentation

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ setup(name='jens',
       version='0.22',
       description='Jens is a Puppet modules/hostgroups librarian',
       classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
       ],
       author='Nacho Barrientos',
       author_email='nacho.barrientos@cern.ch',
       url='https://github.com/cernops/jens',
-      package_dir= {'': 'src'},
-      packages=['jens', 'jens.webapps'],
-      scripts=['bin/jens-update', 'bin/jens-stats',
-        'bin/jens-gitlab-producer-runner', 'bin/jens-purge-queue',
-        'bin/jens-reset', 'bin/jens-gc', 'bin/jens-config'],
+      package_dir={'': 'src'},
+      packages=[
+          'jens', 'jens.webapps'
+      ],
+      scripts=[
+          'bin/jens-update', 'bin/jens-stats',
+          'bin/jens-gitlab-producer-runner', 'bin/jens-purge-queue',
+          'bin/jens-reset', 'bin/jens-gc', 'bin/jens-config'
+      ],
      )
