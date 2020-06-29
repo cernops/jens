fb6e9f59cfeda11d738cc068f50221ad8a71d709
Nacho Barrientos <nacho.barrientos@cern.ch>
2020-06-29 14:07:42 +0200

    Want python3 via the shebang

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 from __future__ import absolute_import
 from distutils.core import setup
--- src/jens/webapps/gitlabproducer.py
+++ src/jens/webapps/gitlabproducer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # Copyright (C) 2015, CERN
 # This software is distributed under the terms of the GNU General Public
 # Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
